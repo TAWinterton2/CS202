e41b8e148fe33ac683106c6c51bfebf46fe83cf7
Travis Winterton <tawinterton2@alaska.edu>
2021-02-05 03:00:54 -0900

    added sorted vector to search

--- Homeworks/Homework 1/Timeit1/Stopwatch.cpp
+++ Homeworks/Homework 1/Timeit1/Stopwatch.cpp
@@ -46,16 +46,14 @@ void Stopwatch::Stop() {
 //Time_Seconds (Calculates total time elapsed in Seconds)
 void Stopwatch::Time_Seconds(){
 	auto diff = t_end - t_start;
-	cout << std::chrono::duration<double>(diff).count();
+	cout << std::chrono::duration<double>(diff).count() << "s\n";
 	std::time_t end_time = std::chrono::system_clock::to_time_t(t_end);
-	cout << "Finished computation at " << std::ctime(&end_time) << '\n';
-	cout << "Elapsed time: " << diff.count() << "s\n";
 }
 
 //Time_Milliseconds (Calculates total time elapsed in Miliseconds)
 
 void Stopwatch::Time_Milliseconds(){
 	auto diff = t_end - t_start;
-	cout << std::chrono::duration<double, std::milli>(diff).count();
+	cout << std::chrono::duration<double, std::milli>(diff).count() << "Ms\n";
 }
 
--- Homeworks/Homework 1/Timeit1/main.cpp
+++ Homeworks/Homework 1/Timeit1/main.cpp
@@ -22,7 +22,7 @@ using std::random_device;
 int gen() {
 	random_device rd;
 	mt19937 mt(rd()); //PRNG - unpredictable seed
-	std::uniform_int_distribution<int> dist(0, 10); // range from 0 to 50 
+	std::uniform_int_distribution<int> dist(0, 5); // range from 0 to 50 
 	static int i = dist(mt);
 	return i;
 }
@@ -33,17 +33,15 @@ int main() {
 	//Declare and generate PRNGS and timers for each search algorithm
 	
 
-	Stopwatch timer_1;
-	Stopwatch timer_2;
-	Stopwatch timer_3;
-	Stopwatch timer_4;
+	Stopwatch timer;
 	
-	timer_2.Start();
+	
+	
 
 	bool result;
 	random_device rd;
 	mt19937 mt(rd()); //PRNG - unpredictable seed
-	std::uniform_int_distribution<int> dist(0, 10); // range from 0 to 50 
+	std::uniform_int_distribution<int> dist(0, 5); // range from 0 to 50 
 
 	//create vector of 10 numbers 
 	vector<int> data(10);
@@ -58,39 +56,62 @@ int main() {
 	while (cin.get() != '\n');
 
 
-	timer_1.Start();
+	timer.Start();
 	result = std::binary_search(data.begin(), data.end(), numbertofind);
-	timer_1.Stop();
+	timer.Stop();
 	cout << "Search Finished, Press enter to display times " << endl;
 	while (cin.get() != '\n');
 
 	if (result == true) {
 		cout << "Element " << numbertofind << "Exist in Vector." << endl;
 		cout << "Time taken: ";
-		timer_1.Time_Seconds();
+		timer.Time_Seconds();
 		cout << endl;
-		timer_1.Time_Milliseconds();
+		timer.Time_Milliseconds();
 		cout << endl;
 
 	}
 	if (result == false) {
 		cout << "Element " << numbertofind << " Doesn't exist in vector. " << endl;
 		cout << "Time taken: ";
-		timer_1.Time_Seconds();
+		timer.Time_Seconds();
 		cout << endl;
-		timer_1.Time_Milliseconds();
+		timer.Time_Milliseconds();
 		cout << endl;
 	}
 
+	//Sorted vector
+	cout << "Sorted" << endl;
+	std::sort(data.begin(), data.end());
+	timer.Start();
+	result = std::binary_search(data.begin(), data.end(), numbertofind);
+	timer.Stop();
+	cout << "Search Finished, Press enter to display times " << endl;
+	while (cin.get() != '\n');
 
+	if (result == true) {
+		cout << "Element " << numbertofind << "Exist in Vector." << endl;
+		cout << "Time taken: ";
+		timer.Time_Seconds();
+		cout << endl;
+		timer.Time_Milliseconds();
+		cout << endl;
+
+	}
+	if (result == false) {
+		cout << "Element " << numbertofind << " Doesn't exist in vector. " << endl;
+		cout << "Time taken: ";
+		timer.Time_Seconds();
+		cout << endl;
+		timer.Time_Milliseconds();
+		cout << endl;
+	}
 	//Search
+	
 
 	while (cin.get() != '\n');
 	
-	timer_2.Stop();
-	cout << "hey!" << endl;
-	timer_2.Time_Seconds();
-
+	
 
 	return 0;
 
