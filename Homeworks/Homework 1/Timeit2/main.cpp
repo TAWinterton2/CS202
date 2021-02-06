3014b8bb29f163d868ec7cbac9ead0ec8b9a13ec
Travis Winterton <tawinterton2@alaska.edu>
2021-02-05 16:46:44 -0900

    Test for finding text completed

--- Homeworks/Homework 1/Timeit2/main.cpp
+++ Homeworks/Homework 1/Timeit2/main.cpp
@@ -19,18 +19,22 @@ using std::fstream;
 using std::ifstream;
 
 int main() {
-
+	//string text and text to find in book 
 	std::string text;
-	std::string text2find = "The holy Creator usward sent him,";
+	std::string text2find = "{I put a stop to the outrages of the sea-monsters.}";
 
+	//Stopwatch class called 
 	Stopwatch timer;
 
+	//vector to read in text file
 	vector<string> booktxt;
 	cout << "Reading in Beowulf.txt" << endl;
 	cout << "Press enter to begin" << endl;
 	while (cin.get() != '\n');
 
 	timer.Start();
+
+	//read in file and push string into vector
 	ifstream Book("Beowulf.txt");
 
 	if (!Book) {
@@ -42,6 +46,8 @@ int main() {
 		}
 	}
 	timer.Stop();
+
+
 	cout << "Finished Reading in Text file" << endl;;
 	cout << "Elasped time seconds: ";
 	timer.Time_Seconds();
@@ -50,8 +56,11 @@ int main() {
 	timer.Time_Milliseconds();
 
 	while (cin.get() != '\n');
+
+	//find text in from book
 	cout << "Find a string of text in Book " << endl;
 	cout << "Press Enter to begin" << endl;
+	while (cin.get() != '\n');
 	timer.Start();
 	if (std::find(booktxt.begin(), booktxt.end(), text2find) != booktxt.end()) {
 		cout << "Text found!" << endl;
