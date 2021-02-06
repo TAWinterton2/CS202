f9ef2653c16b3591e2499176b085a6c98e7a7e89
Travis Winterton <tawinterton2@alaska.edu>
2021-02-05 16:32:40 -0900

    addd txt for beowulf

--- Homeworks/Homework 1/Timeit2/main.cpp
+++ Homeworks/Homework 1/Timeit2/main.cpp
@@ -19,13 +19,16 @@ using std::fstream;
 using std::ifstream;
 
 int main() {
+
 	std::string text;
+	Stopwatch timer;
 
 	vector<string> booktxt;
 	cout << "Reading in Beowulf.txt" << endl;
 	cout << "Press enter to begin" << endl;
 	while (cin.get() != '\n');
 
+	timer.Start();
 	ifstream Book("Beowulf.txt");
 
 	if (!Book) {
@@ -36,9 +39,16 @@ int main() {
 			booktxt.push_back(text);
 		}
 	}
+	timer.Stop();
+	cout << "Finished Reading in Text file" << endl;;
+	cout << "Elasped time seconds: ";
+	timer.Time_Seconds();
+	cout << endl;
+	cout << "Elapsed time Milliseconds: ";
+	timer.Time_Milliseconds();
 
-
-	
-
+	for (auto a : booktxt) {
+		cout << a << ' ';
+	}
 
 }
