747396d35b685cc5463f12c64497524c6d29cf45
Travis Winterton <tawinterton2@alaska.edu>
2021-02-05 16:23:18 -0900

    added code to push back string from beowulf.txt into file

--- Homeworks/Homework 1/Timeit2/main.cpp
+++ Homeworks/Homework 1/Timeit2/main.cpp
@@ -12,6 +12,7 @@
 
 using std::endl;
 using std::cout;
+using std::cin;
 using std::string;
 using std::vector;
 using std::fstream;
@@ -20,13 +21,22 @@ using std::ifstream;
 int main() {
 	std::string text;
 
-	vector<string> book;
+	vector<string> booktxt;
+	cout << "Reading in Beowulf.txt" << endl;
+	cout << "Press enter to begin" << endl;
+	while (cin.get() != '\n');
 
 	ifstream Book("Beowulf.txt");
 
 	if (!Book) {
 		cout << "Error in opening file" << endl;
 	}
+	else {
+		while (std::getline(Book, text)) {
+			booktxt.push_back(text);
+		}
+	}
+
 
 	
 
