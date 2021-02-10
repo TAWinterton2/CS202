e7a5c081c3c0014cc23232ddea404af27e148511
Travis Winterton <tawinterton2@alaska.edu>
2021-02-10 00:12:07 -0900

    3 nodes created for linked list Value*

--- Homeworks/Homework 2/Linked List/Value.cpp
+++ Homeworks/Homework 2/Linked List/Value.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
-
+#include<list>
+#include<string>
+using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
 
 struct Value {
+	string booktitle;
+	
+	Value* next;
+
+
 
 };
--- Homeworks/Homework 2/Linked List/main.cpp
+++ Homeworks/Homework 2/Linked List/main.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#include<list>
+#include"Value.cpp"
+
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main() {
 
-	
-	
+	//3 nodes
+	Value* head = NULL;
+	Value* second = NULL;
+	Value* thrid = NULL;
+
 
 
 	return 0;
