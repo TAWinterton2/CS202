8ab2a115df842492ffc38876c57dc0d6bef70240
Travis Winterton <tawinterton2@alaska.edu>
2021-02-11 00:37:31 -0900

    Removed value.cpp, moved declaration of Value into main

--- Homeworks/Homework 2/Linked List/main.cpp
+++ Homeworks/Homework 2/Linked List/main.cpp
@@ -7,11 +7,52 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+list<Value> testlist;
+list<Value>::iterator it;
+
+struct Value {
+	string booktitle;
+	string author;
+	int publicationdate = 0;
+};
+
+
+//prints contents of linked list, starting w/ given node
+void printlist(list <Value>& V) {
+
+	list <Value> ::iterator it;
+	for (it = V.begin(); it != V.end(); ++it) {
+		cout << '\t' << it->booktitle << endl;
+		cout << '\t' << it->booktitle << endl;
+		cout << "\t Publication date: " << it->publicationdate << endl;
+		cout << endl;
+	}
+
+}
 TEST_CASE("Queue: First in First out") {
+	Value book1, book2;
+	book1.booktitle = "The Hobbit";
+	book1.author = "J.R Tolkien";
+	book1.publicationdate = 1937;
+
+	testlist.push_front(book1);
+
+	book2.booktitle = "The Felloship of the Ring";
+	book2.author = "J.R Tolkien";
+	book2.publicationdate = 1943;
+
+	testlist.push_back(book2);
+	
+	REQUIRE(testlist.front().booktitle == book1.booktitle);
+
+
+
+
+
 
 
 }
-
+/*
 TEST_CASE("Stack: Last in First out") {
 
 }
@@ -22,5 +63,5 @@ TEST_CASE("INSERT AND FIND") {
 }
 
 TEST_CASE("Print out list") {
-
-}
+	
+}*/
