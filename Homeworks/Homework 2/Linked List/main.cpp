3c4b3645fce6216c615873edc54b34469f075cc5
Travis Winterton <tawinterton2@alaska.edu>
2021-02-11 15:44:42 -0900

    Redid declaration of Book1 in TEST_CASE queue

--- Homeworks/Homework 2/Linked List/main.cpp
+++ Homeworks/Homework 2/Linked List/main.cpp
@@ -29,7 +29,7 @@ void printlist(list <Value>& V) {
 	list <Value>::iterator it;
 	for (it = V.begin(); it != V.end(); ++it) {
 		cout << '\t' << it->booktitle << endl;
-		cout << '\t' << it->booktitle << endl;
+		cout << '\t' << it->author << endl;
 		cout << "\t Publication date: " << it->publicationdate << endl;
 		cout << endl;
 	}
@@ -45,28 +45,25 @@ void printlist(list <Value>& V) {
 TEST_CASE("Queue: First in First out") {
 	list<Value> testlist;
 
-	Value book1, book2;
-	book1.booktitle = "The Hobbit";
-	book1.author = "J.R Tolkien";
-	book1.publicationdate = "1937";
+	Value book1{ "The Hobbit", "J.R Tolkien", "1937" }, book2{ "The Fellowship of the Ring" " J.R Tolkien", "1943" };
+	
 
-
+	//book1 is first element of list
 	testlist.push_back(book1);
 
-	book2.booktitle = "The Fellowship of the Ring";
-	book2.author = "J.R Tolkien";
-	book2.publicationdate = "1943";
 
+	//book 2 is second element of list
 	testlist.push_back(book2);
 	
-	//Test to see that first element of testlist is The hobbit
+	//Test to see that first element of testlist is book1
 	REQUIRE(testlist.front().booktitle == book1.booktitle);
 	REQUIRE(testlist.front().author == book1.author);
 	REQUIRE(testlist.front().publicationdate == book1.publicationdate);
 	
-	//remove first element of testlist
+	//remove first element of testlist, making book2 the first element of list
 	testlist.pop_front();
 
+	//test to see the first element of book list is now book 2
 	REQUIRE(testlist.front().booktitle == book2.booktitle);
 	REQUIRE(testlist.front().author == book2.author);
 	REQUIRE(testlist.front().publicationdate == book2.publicationdate);
@@ -86,15 +83,17 @@ TEST_CASE("Stack: Last in First out") {
 	book1.author = "Brandon Sanderson";
 	book1.publicationdate = "2010";
 
+	//make book1 last element of list 
 	testlist.push_front(book1);
 
 	book2.booktitle = "Worlds of Radiance";
 	book2.author = "Brandon Sanderson";
 	book2.publicationdate = "2014";
 
+	//book 2 is now first element of list
 	testlist.push_front(book2);
 
-	//Test to see that first element of testlist is The hobbit
+	//Test to see that first element of testlist is book 2
 	REQUIRE(testlist.front().booktitle == book2.booktitle);
 	REQUIRE(testlist.front().author == book2.author);
 	REQUIRE(testlist.front().publicationdate == book2.publicationdate);
@@ -102,10 +101,12 @@ TEST_CASE("Stack: Last in First out") {
 	//remove first element of testlist
 	testlist.pop_front();
 
+	//test to see that first element of testlist is now book 1
 	REQUIRE(testlist.front().booktitle == book1.booktitle);
 	REQUIRE(testlist.front().author == book1.author);
 	REQUIRE(testlist.front().publicationdate == book1.publicationdate);
 }
+
 /*
 TEST_CASE("INSERT AND FIND") {
 
