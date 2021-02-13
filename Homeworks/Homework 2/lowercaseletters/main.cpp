e54756c2f9ce0d6120a351f3f7cb5ee3270b8e69
Travis Winterton <tawinterton2@alaska.edu>
2021-02-12 21:06:42 -0900

    added code to main function to get string

--- Homeworks/Homework 2/lowercaseletters/main.cpp
+++ Homeworks/Homework 2/lowercaseletters/main.cpp
@@ -2,10 +2,35 @@
 #include<string>
 #include<stdio.h>
 
-void to_lower(char* string) {
-	int j, k;
 
-	for (j = 0; string[j] != '\0'; j++) {
+//function to convert string to lower case
+void to_lower(char* string) {
+	//counters to count thorugh each char
+	int counter1, counter2;
+	
+	//check to see if letter is already lowercase
+	for (counter1 = 0; string[counter1] != '\0'; counter1++)
+	{
 
 	}
+	//convert Letter to uppercase 
+	for (counter2 = 0; counter2 <= counter1; ++counter2) {
+		if (string[counter1] >= 65 && string[counter1] <= 92) {
+			string[counter1] = string[counter1] + 32;
+		}
+	}
+	std::cout << "Converted: " << string;
 }
+
+
+int main() {
+	char teststring[10], * a;
+
+	std::cout << "Enter String w/ uppercase letters: ";
+	std::gets(teststring);
+
+	to_lower(teststring);
+
+	return 0;
+}
+
