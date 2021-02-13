d0fe3b17496aab736ec62eac925c287b36a19fff
Travis Winterton <tawinterton2@alaska.edu>
2021-02-12 16:55:02 -0900

    removed printpointer function

--- Homeworks/Homework 2/Memory layout/main.cpp
+++ Homeworks/Homework 2/Memory layout/main.cpp
@@ -16,25 +16,37 @@ int global1, global2, global3;
 
 
 
-//prints pointer of variable 
-void printPointer(int i ){
-	int* iptr = &i;
-	cout << "Value " << *iptr << endl;
-	cout << "Adress: " << iptr << endl;
-}
-
 int main() { //main goes into stack
 
 	int local1, local2, local3 ; //local variable - goes into stack
-	
+
+	std::vector<long> memorylocations;
 
 	cout << "Address of global1: " << (long)&global1 << endl;
+	memorylocations.push_back((long)&global1);
 	cout << "Address of global2: " << (long)&global2 << endl;
+	memorylocations.push_back((long)&global2);
 	cout << "Address of global3: " << (long)&global3 << endl;
+	memorylocations.push_back((long)&global3);
 	cout << "Address of Local1:  " << (long)&local1 << endl;
+	memorylocations.push_back((long)&local1);
 	cout << "Address of Local2:  " << (long)&local2 << endl;
+	memorylocations.push_back((long)&local2);
 	cout << "Address of Local3:  " << (long)&local3 << endl;
+	memorylocations.push_back((long)&local3);
 
+	std::sort(memorylocations.begin(), memorylocations.end());
+	
+	cout << "Memory order: " << endl;
+
+	for (int i = 0; i < memorylocations.size();  i++) {
+		cout << memorylocations[i] << endl;
+
+	}
+
+	cout << "Stack Grows towards higher addresses" << endl;
+
+	cout << "Adding in free store" << endl;
 
 
 	return 0;
