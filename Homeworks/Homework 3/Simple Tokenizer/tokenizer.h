2a1f645b7223560b0453441d6151e07261980380
Travis Winterton <tawinterton2@alaska.edu>
2021-02-23 12:39:20 -0900

    defined function anaylze tokens

--- Homeworks/Homework 3/Simple Tokenizer/tokenizer.h
+++ Homeworks/Homework 3/Simple Tokenizer/tokenizer.h
@@ -23,6 +23,16 @@ bool Readline(string& str);
 // push a blank string to the end of each line. Return # of tokens read form the string
 unsigned StringToTokens(const string& input, vector<string>& tokens);
 
+//takes a vector and determines if the toke is :
+	// an interger literal (all characters are numbers)
+	// an identifier literal (starts w/ a letter of underscore character and followed by A-Z, a-z, 0-9)
+	// a string literal (starts and ends with double quotes)
+	// a white space literal (blank string)
+	// an unkown ( undetermined from the input)
+// also prints out the token itself surrounded by quotation marks.
+void AnalyzeTokens(vector<string> tokens);
+
+
 
 
 
