9afd30e35e1f6018c549c38a35c626fa1e0311e0
Travis Winterton <tawinterton2@alaska.edu>
2021-02-23 10:45:55 -0900

    created header file for functions for simple tokens program

---
+++ Homeworks/Homework 3/Simple Tokenizer/tokenizer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+#ifndef FILE_TOKENIZER_H
+#define FILE_TOKENIZER_H
+
+
+
+
+
+
+
+#endif
