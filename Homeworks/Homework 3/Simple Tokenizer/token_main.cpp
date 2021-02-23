2d4aceaed355f003ef2c72c7a28f41e692b1a92e
Travis Winterton <tawinterton2@alaska.edu>
2021-02-23 12:28:31 -0900

    created functions bool readline ansd stringtotokens

--- Homeworks/Homework 3/Simple Tokenizer/token_main.cpp
+++ Homeworks/Homework 3/Simple Tokenizer/token_main.cpp
@@ -14,5 +14,14 @@ using std::string;
 using std::vector;
 
 int main() {
+	string user_input;
+	vector <string> tokens;
+
+	//get input from user or text file
+	cout << "Enter in some text. When you are done, type 'End', 'end' or 'END' ";
+	cout << "\n";
+
+	
+	return 0;
 
 }
--- Homeworks/Homework 3/Simple Tokenizer/tokenizer.h
+++ Homeworks/Homework 3/Simple Tokenizer/tokenizer.h
@@ -14,8 +14,14 @@ using std::vector;
 #ifndef FILE_TOKENIZER_H
 #define FILE_TOKENIZER_H
 
+//uses getline() to get in text from user or txt file
+//return true if the read the string and false if the string was a blank line 
+bool Readline(string& str);
 
 
+//uses std::istringstream to read strings seperated by whitespace characets. 
+// push a blank string to the end of each line. Return # of tokens read form the string
+unsigned StringToTokens(const string& input, vector<string>& tokens);
 
 
 
