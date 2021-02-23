e157d8936bcb87278b46b5380159ccb770037e7c
Travis Winterton <tawinterton2@alaska.edu>
2021-02-23 12:55:18 -0900

    bool Readline defined

--- Homeworks/Homework 3/Simple Tokenizer/tokenizer.cpp
+++ Homeworks/Homework 3/Simple Tokenizer/tokenizer.cpp
@@ -12,3 +12,18 @@ using std::string;
 using std::vector;
 
 
+//return true if it read the string and false if the string was a blank line 
+bool Readline(string str) {
+	getline(cin, str);
+
+	if (str.empty()) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+
+
