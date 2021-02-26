0640a2c7a0111612f7537436308eab7fc617e8e5
Travis Winterton <tawinterton2@alaska.edu>
2021-02-25 15:33:58 -0900

    defined Default, copy, and destructor for money class

--- Homeworks/Homework 3/Money Class/Money.h
+++ Homeworks/Homework 3/Money Class/Money.h
@@ -2,9 +2,22 @@
 #ifndef MONEY_HPP
 #define MONEY_HPP
 
+#include<iostream>
+#include<iomanip>
+
+
 
 class Money
 {
+	//default constructor 
+	Money(double amount = 0.00);
+
+	//copy constructor
+	Money(const Money& old);
+
+	//destructor 
+	~Money();
+
 
 
 };
