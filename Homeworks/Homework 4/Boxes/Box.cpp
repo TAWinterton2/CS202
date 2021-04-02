aeae102a0fb6acd0581b3ca2382078ea2c44f975
Travis Winterton <tawinterton2@alaska.edu>
2021-04-01 16:22:36 -0800

    default constructor for box and constructor w/ width and height created

--- Homeworks/Homework 4/Boxes/Box.cpp
+++ Homeworks/Homework 4/Boxes/Box.cpp
@@ -1,1 +1,14 @@
 #include "Box.h"
+
+
+
+
+//default constructor 
+Box::Box() {
+
+}
+
+//constructor w/ width and hieght
+Box::Box(int width, int hieght) : _width(width), _hieght(hieght) {
+
+}
--- Homeworks/Homework 4/Boxes/Box.h
+++ Homeworks/Homework 4/Boxes/Box.h
@@ -2,8 +2,18 @@
 class Box
 {
 public:
+	//default constructor
+	Box();
+
+	//constructor w/ width and height
+	Box(int width, int hieght);
+
 
 private:
+	int _width;
+
+	int _hieght;
+
 
 };
 
