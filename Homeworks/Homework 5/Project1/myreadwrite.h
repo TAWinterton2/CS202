d214526be113d331b16e87f685d20491a510bae7
Travis Winterton <tawinterton2@alaska.edu>
2021-04-24 01:23:20 -0800

    defined template specalization for myWrite

--- Homeworks/Homework 5/Project1/myreadwrite.h
+++ Homeworks/Homework 5/Project1/myreadwrite.h
@@ -14,6 +14,7 @@ using std::string;
 using std::ofstream;
 using std::ifstream;
 using std::ios;
+using std::fstream;
 
 #ifndef MY_READ_WRITE_H
 #define MY_READ_WRITE_H
@@ -21,9 +22,21 @@ using std::ios;
 
 //template for myWrite
 template <typename T>
-T myWrite(T t, T t2) {
+T myWrite(ofstream file, T t) {
+	file >> t;
+	return("Contents Saved");
+}
+
+//template specalization
+ int myWrite(ofstream file, int t) {
+	 file.write(reinterpret_cast<const char*>(&t), sizeof(t));
 
 }
+
+ double myWriate(ofstream file, double t) {
+
+ }
+
 
 //template for myRead
 template <typename T>
