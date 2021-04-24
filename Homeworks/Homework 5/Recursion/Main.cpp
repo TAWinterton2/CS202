581d9c53c73d8f8ffac92bddbb974a1facca4a5f
Travis Winterton <tawinterton2@alaska.edu>
2021-04-24 03:19:08 -0800

    completed code for fib_loop

--- Homeworks/Homework 5/Recursion/Main.cpp
+++ Homeworks/Homework 5/Recursion/Main.cpp
@@ -10,16 +10,39 @@ int fib(int n) {
 }
 
 int fib_loop(int n) {
-	for (int x = 1; x <= n; x++) {
+	int t1 = 0;
+	int t2 = 1;
+	int nT;
 
+	for (int x = 1; x <= n; x++) {
+		std::cout << t1 << " ";
+		nT = t1 + t2;
+		t1 = t2;
+		t2 = nT;
 
 	}
+	std::cout << std::endl;
+	return 0;
 }
 
 int main() {
 	
+	int j = 0;
+
+	std::cout << fib(9) << std::endl;
+
+	while (j < 9) {
+		std::cout << ' ' << fib(j);
+		j++;
+
+	}
+	std::cout << std::endl;
+
 	std::cout << fib(9);
 
+	std::cout << std::endl;
+
+	std::cout << fib_loop(9) << std::endl;
 	
 	return 0;
 
