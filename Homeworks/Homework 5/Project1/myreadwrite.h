5f6320d2651f927a0827095061fdc04f5a7cd8e0
Travis Winterton <tawinterton2@alaska.edu>
2021-04-23 23:59:28 -0800

    hw 5 created, defined tmeplate for mywraite

---
+++ Homeworks/Homework 5/Project1/myreadwrite.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+
+#ifndef MY_READ_WRITE_H
+#define MY_READ_WRITE_H
+
+
+//template for myWrite
+template <typename T>
+T myWrite{ T x, T y }{
+
+}
+
+
+
+
+#endif
