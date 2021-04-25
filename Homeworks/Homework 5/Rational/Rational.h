33e5e48307b7049a9a43a0e59f2f63ead3c1dfa8
Travis Winterton <tawinterton2@alaska.edu>
2021-04-24 23:25:52 -0800

    included function defintions in hpp

--- Homeworks/Homework 5/Rational/Rational.h
+++ Homeworks/Homework 5/Rational/Rational.h
@@ -8,70 +8,127 @@
 // _denominator is always > 0
 template <typename T> class Rational {
 	template <typename U>
-	friend std::ostream& operator<<(std::ostream&, const Rational<T>& rhs);
+	friend std::ostream& operator<<(std::ostream&, const Rational<T>& rhs) {
+		os << rhs._numerator;
+		if (rhs._denominator != 1)
+			os << "/" << rhs._denominator;
+		return os;
+	}
 
 	template<typename U>
-	friend Rational<U> operator+(const Rational<U>& lhs, const Rational<U>& rhs);
+	friend Rational<U> operator+(const Rational<U>& lhs, const Rational<U>& rhs) {
+		auto temp{ lhs };
+		temp += rhs;
+		return temp;
+	}
 
 	template<typename U>
-	friend Rational<U> operator-(const Rational<U>& lhs);
+	friend Rational<U> operator-(const Rational<U>& lhs) {
+		return { -lhs._numerator, lhs._denominator };
+	}
 
 	template<typename U>
-	friend bool operator==(const Rational<U>& lhs, const Rational<U>& rhs);
+	friend bool operator==(const Rational<U>& lhs, const Rational<U>& rhs) {
+		return lhs._numerator == rhs._numerator && lhs._denominator == rhs._denominator;
+	}
 
 	template<typename U>
-	friend bool operator<(const Rational<U>& lhs, const Rational<U>& rhs);
+	friend bool operator<(const Rational<U>& lhs, const Rational<U>& rhs) {
+		return lhs._numerator * rhs._denominator < rhs._numerator* lhs._denominator;
+	}
 
 public:
-	
-	Rational(int, int = 1); 
+
+	Rational(int, int = 1);
 
 	template<typename U>
-	Rational& operator+=(const Rational<U>& rhs);
-	
-	template<typename U>
-	Rational& operator-=(const Rational<U>& rhs);
-	
-	template<typename U>
-	Rational& operator*=(const Rational<U>& rhs);
+	Rational& operator+=(const Rational<U>& rhs) {
+		_numerator = _numerator * rhs._denominator + rhs._numerator * _denominator;
+		_denominator *= rhs._denominator;
+		reduce();
+		return *this;
+	}
 
 	template<typename U>
-	Rational& operator/=(const Rational<U>& rhs);
+	Rational& operator-=(const Rational<U>& rhs) {
+		*this = *this - rhs; //uses Rational::operator- to define operator-=
+		return *this;
+	}
 
 	template<typename U>
-	Rational& operator++();        
+	Rational& operator*=(const Rational<U>& rhs) {
+		_numerator *= rhs._numerator;
+		_denominator *= rhs._denominator;
+		return *this;
+	}
+
+	template<typename U>
+	Rational& operator/=(const Rational<U>& rhs) {
+		return *this *= {rhs._denominator, rhs._numerator};
+	}
+
+	template<typename U>
+	Rational& operator++() {
+		return *this += 1;
+	}
 
 	template <typename U>
-	Rational operator++(int); 
+	Rational operator++(int) {
+		auto copy{ *this };
+		++(*this);
+		return copy;
+	}
 
 	template <typename U>
-	Rational& operator--();       
+	Rational& operator--(); {
+		return *this -= 1;
+	}
 
 	template <typename U>
-	Rational operator--(int); 
+	Rational operator--(int) {
+		auto copy{ *this };
+		--(*this);
+		return copy;
+	}
+
+
 	void reduce();
 
-	T * _numerator;
-	T * _denominator;
+	T* _numerator;
+	T* _denominator;
 };
 
 template<typename T>
-T operator-(const Rational<T>& lhs, const Rational<T>& rhs);
+T operator-(const Rational<T>& lhs, const Rational<T>& rhs) {
+	return lhs + -rhs;
+}
 
 template<typename T>
-T operator*(Rational<T> lhs, const Rational<T>& rhs);
+T operator*(Rational<T> lhs, const Rational<T>& rhs) {
+	return lhs *= rhs;
+}
 
 template<typename T>
-T operator/(Rational<T> lhs, const Rational<T> & rhs);
+T operator/(Rational<T> lhs, const Rational<T>& rhs) {
+	return lhs /= rhs;
+}
+
 
 template<typename T>
-T operator>(const Rational<T>& lhs, const Rational<T>& rhs);
+T operator>(const Rational<T>& lhs, const Rational<T>& rhs) {
+	return rhs < lhs;
+}
 
 template<typename T>
-T operator<=(const Rational<T>& lhs, const Rational<T>& rhs);
+T operator<=(const Rational<T>& lhs, const Rational<T>& rhs) {
+	return !(rhs > lhs);
+}
 
 template<typename T>
-T operator>=(const Rational<T>& lhs, const Rational<T>& rhs);
+T operator>=(const Rational<T>& lhs, const Rational<T>& rhs) {
+	return !(rhs < lhs);
+}
+
 
 
 
